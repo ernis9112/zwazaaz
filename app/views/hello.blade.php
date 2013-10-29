ddeb9e0a8d079cef54fc97eafbcf38a7e083d50d
Ernesto <Ernesto@192.168.0.101>
2013-10-29 20:46:18 +0200

    Prisijungimas. Labai daug dar netestavau, tai jau kur buguosis - sakykit.

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,14 +1,14 @@
-<form>
+{{ Form::open(array('route' => 'login.try', 'id' => 'login')) }}
     <div class="row">
         <div class="col-sm-12 text-center">
-            <a class="logo-link" href="#">
+            <a class="logo-link" href="{{ route('hello') }}">
                 <img src="assets/img/logo.png" alt="Zwazaaz">
             </a>
         </div>
     </div>
     <div class="row">
         <div class="col-sm-12">
-            <div class="alert alert-danger">
+            <div class="alert alert-danger"  @if (!Session::get('tried_login')) style="display:none" @endif >
                 <strong>Oh snap!</strong> We didn't recognize your sign-in details. Please check your Zwazaaz Name and password, then try again.
             </div>
         </div>
@@ -17,11 +17,11 @@
         <div class="col-sm-6">
             <div class="form-group">
                 <label class="control-label" for="sign-in-name">Zwazaaz Name</label>
-                <input type="text" class="form-control" id="sign-in-name" value="zwazaazius" required>
+                <input name="username" type="text" class="form-control" id="sign-in-name" value="@if (Session::get('tried_login')){{ Session::get('tried_login') }}@endif" required>
             </div>
             <div class="form-group">
                 <label class="control-label" for="sign-in-password">Password</label>
-                <input type="password" class="form-control" id="sign-in-password" value="sgasdgasdghahf" required>
+                <input name="password" type="password" class="form-control" id="sign-in-password" value="" required>
             </div>
             <div class="form-group btn-group" data-toggle="buttons">
                 <label class="btn custom-checkbox">
@@ -30,7 +30,7 @@
             </div>
             <div class="row">
                 <div class="col-sm-12">
-                    <button type="submit" class="btn btn-default">Sign in</button>&nbsp;or&nbsp;<a href="#">Create an account</a>
+                    <button type="submit" class="btn btn-default">Sign in</button>&nbsp;or&nbsp;<a href="{{ route('registration') }}">Create an account</a>
                 </div>
             </div>
         </div>
@@ -46,4 +46,5 @@
             </div>
         </div>
     </div>
-</form>
+{{ Form::token() }}
+{{ Form::close() }}
