1a104a4270498a3104563824de0772ddfeff8361
Simas Skilinskas <simskil@gmail.com>
2013-11-13 21:05:53 +0200

    Removed "Rokas Budnikas" demo profile from dashboard page

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -147,46 +147,13 @@
 </aside>
 <div class="main-content">
 
-    <div class="profile-view">
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="profile-img">
-                    <div class="profile-img-wrapper">
-                        <img src="assets/img/user-blank.jpg" alt="UserName">
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="profile-info-row row">
-                    <div class="info-label col-md-5">
-                        Name
-                    </div>
-                    <div class="info-value col-md-7">
-                        Rokas Budnikas
-                    </div>
-                </div>
-                <div class="profile-info-row row">
-                    <div class="info-label col-md-5">
-                        Zwazaaz name
-                    </div>
-                    <div class="info-value col-md-7">
-                        rokasbud
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="contact-actions">
-                    <a href="#" class="action btn btn-success btn-lg"><i class="glyphicon glyphicon-earphone"></i><span class="text">Call</span></a>
-                </div>
-                <div class="contact-actions pull-right">
-                    <a href="#" class="action btn btn-warning btn-xs"><i class="glyphicon glyphicon-trash"></i><span class="text">Remove</span></a>
-                    <a href="#" class="action btn btn-danger btn-xs"><i class="glyphicon glyphicon-ban-circle"></i><span class="text">Block</span></a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <article>
+        <h1>Zwazaaz</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia ac odio vulputate mattis. Maecenas
+            ligula nulla, vehicula quis feugiat sed, pellentesque quis mi. Curabitur eu eleifend eros. Vivamus porttitor
+            lacus ut turpis posuere gravida. Pellentesque luctus a massa sit amet malesuada. Aenean eget arcu ut nulla
+            semper venenatis at faucibus leo.</p>
+    </article>
 
     <div class="web-cam-wrapper">
         <div class="video-container friend-camera va-middle" id="remoteVideos">
