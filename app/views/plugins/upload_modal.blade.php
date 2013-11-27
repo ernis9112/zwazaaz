f65ed1f70bc879061d1e4cf7c5741f6a10828ae1
Simas Skilinskas <simskil@gmail.com>
2013-11-27 11:28:59 +0200

    Modal made responsive.

--- app/views/plugins/upload_modal.blade.php
+++ app/views/plugins/upload_modal.blade.php
@@ -1,16 +1,26 @@
-<div style="background-color: white; height: 300; width: 600; margin: 0 auto; margin-top: 100; overflow: hidden" class = "modal" id = "upload_modal">
-    <div class = "modal-header">
-        <button type = "button" class="close" data-dismiss="modal">&times;</button>
-        <h1>Upload your new profile picture!</h1>
-    </div>
-    <div class="modal-body">
-        {{Form::open(array('route' => 'user.upload.pic', 'files'=> true))}}
-        {{Form::file('photo')}}
-    </div>
-    <div class="modal-footer">
-        <a href="#" class="btn" data-dismiss="modal">Cancel</a>
-        <button class="btn btn-primary"> Upload pic</button>
-        {{ Form::token() }}
-        {{ Form::close() }}
-    </div>
-</div>
+<!-- Modal -->
+<div class="modal fade" id="upload_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class = "modal-header">
+                <button type = "button" class="close" data-dismiss="modal">&times;</button>
+                <h1>Upload your new profile picture!</h1>
+            </div>
+            <div class="modal-body">
+                {{Form::open(array('route' => 'user.upload.pic', 'files'=> true))}}
+
+                <div class="form-group">
+                    <label for="exampleInputFile">Choose picture</label>
+                    {{Form::file('photo')}}
+                    <p class="help-block"></p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="btn" data-dismiss="modal">Cancel</a>
+                <button class="btn btn-primary"> Upload pic</button>
+                {{ Form::token() }}
+                {{ Form::close() }}
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
