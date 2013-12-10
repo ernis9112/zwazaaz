2a8c4337d989cbc1d8ad5dbbb13f3fd2a5b97d98
Simas Skilinskas <simskil@gmail.com>
2013-12-11 00:15:06 +0200

    Style fixes, button icons Modal form fix

--- app/views/plugins/upload_modal.blade.php
+++ app/views/plugins/upload_modal.blade.php
@@ -2,13 +2,12 @@
 <div class="modal fade" id="upload_modal" tabindex="-1" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
+            {{Form::open(array('route' => 'user.upload.pic', 'files'=> true))}}
             <div class = "modal-header">
                 <button type = "button" class="close" data-dismiss="modal">&times;</button>
                 <h1>Upload your new profile picture!</h1>
             </div>
             <div class="modal-body">
-                {{Form::open(array('route' => 'user.upload.pic', 'files'=> true))}}
-
                 <div class="form-group">
                     <label>Choose picture</label>
                     {{Form::file('photo')}}
@@ -18,9 +17,9 @@
             <div class="modal-footer">
                 <a href="#" class="btn" data-dismiss="modal">Cancel</a>
                 <button class="btn btn-primary"> Upload pic</button>
-                {{ Form::token() }}
-                {{ Form::close() }}
             </div>
+            {{ Form::token() }}
+            {{ Form::close() }}
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
