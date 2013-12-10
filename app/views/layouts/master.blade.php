f7f9e550bc37747accbb4eda3fc3cf064e6421c4
Simas Skilinskas <simskil@gmail.com>
2013-12-10 21:39:28 +0200

    Favicon

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="{{ asset('assets/bootstrap-3.0.0/assets/ico/favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}">
     <title>Zwazzaz</title>
     <!-- Bootstrap core CSS -->
     {{ HTML::style('assets/bootstrap-3.0.0/dist/css/bootstrap.min.css') }}
